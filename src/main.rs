7469ba60f50439494c0dd771886fa67def8b84c5
Rance Campbell <rance@outlawcoding.com>
2021-10-19 07:37:58 -0600

    change visitor list to vector and add new people to list

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,6 @@
 use std::io::stdin;
 
+#[derive(Debug)]
 struct Visitor {
     name: String,
     greeting: String,
@@ -27,19 +28,34 @@ fn what_is_your_name() -> String {
 }
 
 fn main() {
-    let visitor_list = [
+    let mut visitor_list = vec![
         Visitor::new("rance", "Hello supreme leader"),
         Visitor::new("maddy", "Hi sexy woman"),
         Visitor::new("mason", "Howdy Partner"),
         Visitor::new("marcus", "So cute baby"),
     ];
 
-    println!("Hello, what's your name?");
-    let your_name = what_is_your_name();
-    println!("You said your name is, {}?", &your_name);
-    let known_visitor = visitor_list.iter().find(|v| v.name == your_name);
-    match known_visitor {
-        Some(v) => v.greet_visitor(),
-        None => println!("Get out of here fart sniffer"),
+    loop {
+        println!("Hello, what's your name? (ENTER to quit)");
+        let your_name = what_is_your_name();
+        println!("You said your name is, {}?", &your_name);
+        let known_visitor = visitor_list.iter().find(|v| v.name == your_name);
+        match known_visitor {
+            Some(v) => v.greet_visitor(),
+            None => {
+                if your_name.is_empty() {
+                    break;
+                } else {
+                    println!(
+                        "Sorry {}, I'll add you to the list for the next party.",
+                        your_name
+                    );
+                    visitor_list.push(Visitor::new(&your_name, "What's up bruh"));
+                }
+            }
+        }
     }
+
+    println!("The final list of visitors: ");
+    println!("{:#?}", visitor_list);
 }
